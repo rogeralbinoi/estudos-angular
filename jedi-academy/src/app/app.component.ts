298cc3b595269e4ada9720d6d064d26817394c18
rogeralbinoi <roger.albino.i@gmail.com>
2017-11-15 17:24:31 -0200

    refactor: use ngFor directive

--- jedi-academy/src/app/app.component.ts
+++ jedi-academy/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './student/student.model'
 
 @Component({
   selector: 'jad-root',
@@ -6,17 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
-  luke = {
-    name: 'Luke',
-    isJedi: true,
-    temple: 'Coruscant'
-  }
-  leia = {
-    name: 'Leia',
-    isJedi: false
-  }
-  han = {
-    name: 'Han Solo',
-    isJedi: false
-  }
+  students: Student[] = [
+    {
+      name: 'Luke',
+      isJedi: true,
+      temple: 'Coruscant'
+    },
+    {
+      name: 'Leia',
+      isJedi: false
+    },
+    {
+      name: 'Han Solo',
+      isJedi: false
+    }
+  ]
 }
